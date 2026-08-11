344a5629a77c080e25abbd1a1c0244ab823d827b
oswinvibin07 <oswinvibin07@gmail.com>
2026-08-11 14:52:43 +0530

    Create 1452-RestaurantGrowth

---
+++ 1452-RestaurantGrowth/1452-RestaurantGrowth.sql
@@ -0,0 +1,24 @@
+-- Last updated: 8/11/2026, 2:52:43 PM
+# Write your MySQL query statement below
+SELECT 
+    visited_on,
+    SUM(total_daily_amount) OVER(
+        ORDER BY visited_on 
+        ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+    ) AS amount,
+    
+    
+    ROUND(
+        AVG(total_daily_amount) OVER(
+            ORDER BY visited_on 
+            ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+        ), 
+        2
+    ) AS average_amount
+FROM (
+    
+    SELECT visited_on, SUM(amount) AS total_daily_amount
+    FROM Customer
+    GROUP BY visited_on
+) daily_summary
+LIMIT 6, 999999;
