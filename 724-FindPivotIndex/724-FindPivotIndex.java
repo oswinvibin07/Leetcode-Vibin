09bf33ccd3645a6f70d2f9d0b7cdc0e70dbb7078
oswinvibin07 <oswinvibin07@gmail.com>
2026-08-11 14:54:29 +0530

    Create 724-FindPivotIndex

---
+++ 724-FindPivotIndex/724-FindPivotIndex.java
@@ -0,0 +1,20 @@
+// Last updated: 8/11/2026, 2:54:29 PM
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int total = 0;
+        for (int num : nums) {
+            total += num;
+        }
+        
+        int leftTotal = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int rightTotal = total - leftTotal - nums[i];
+            if (rightTotal == leftTotal) {
+                return i;
+            }
+            leftTotal += nums[i];
+        }
+
+        return -1;        
+    }
+}
