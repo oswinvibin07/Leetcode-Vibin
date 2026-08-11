bb9bb4463e5c9fb4ce9a353acfb8852e3b8b3a28
oswinvibin07 <oswinvibin07@gmail.com>
2026-08-11 14:53:47 +0530

    Create 935-OrderlyQueue

---
+++ 935-OrderlyQueue/935-OrderlyQueue.java
@@ -0,0 +1,19 @@
+// Last updated: 8/11/2026, 2:53:47 PM
+class Solution {
+    public String orderlyQueue(String s, int k) {
+        if(k>1){
+            char tempArray[] = s.toCharArray();
+            Arrays.sort(tempArray);
+            return new String(tempArray);
+        } else {
+            String ans  = s;
+            for(int i=0;i< s.length(); i++){
+                s = s.substring(1) + s.substring(0,1);
+                if(ans.compareTo(s) >0){
+                    ans = s;
+                }
+            }
+            return ans;
+        }
+    }
+}
