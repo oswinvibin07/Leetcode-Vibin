fed8114ca955ea8f1b6f3f58f7bb2245da151d2c
oswinvibin07 <oswinvibin07@gmail.com>
2026-08-11 14:54:50 +0530

    Create 476-NumberComplement

---
+++ 476-NumberComplement/476-NumberComplement.java
@@ -0,0 +1,12 @@
+// Last updated: 8/11/2026, 2:54:50 PM
+class Solution {
+    public int findComplement(int num) {
+        if (num == 0) return 1;
+
+        int bitLength = Integer.toBinaryString(num).length();
+        
+        int mask = (1 << bitLength) - 1;
+        
+        return num ^ mask;
+    }
+}
